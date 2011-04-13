294dc2bb604b784dd48a285d0ae6333755c40603
Nathan Witmer <nwitmer@gmail.com>
2011-04-13 10:34:05 -0600

    Add specs for altered Time.zone_default

--- spec/ar_timezone_spec.rb
+++ spec/ar_timezone_spec.rb
@@ -199,3 +199,83 @@ describe "AR with Time.zone_default set" do
 
 end
 
+describe "AR with Time.zone_default set to Eastern (db is Mountain) and Time.zone set to Eastern" do
+
+  before :all do
+    ENV["TZ"].should == "US/Mountain" # sanity
+    Time.zone_default = Time.__send__(:get_zone, "Eastern Time (US & Canada)")
+    ActiveRecord::Base.default_timezone = :utc # default
+    ActiveRecord::Base.time_zone_aware_attributes = true # default
+  end
+
+  before :each do
+    @time = Time.parse("2011-04-12 11:30:00 -0600")
+    Time.zone = nil
+  end
+
+  context "and default_timezone = :local" do
+    before :all do
+      ActiveRecord::Base.default_timezone = :local
+    end
+
+    context "and time_zone_aware_attributes = true (default)" do
+      before :all do
+        ActiveRecord::Base.time_zone_aware_attributes = true
+        connect_and_define_model
+      end
+
+      it "writes local Time objects to the database as local time (ok)" do
+        item = @items.create :ts => @time
+        item.reload.ts_before_type_cast.should == "2011-04-12 11:30:00.000000"
+      end
+
+      it "writes local UTC Time objects as UTC (ok)" do
+        item = @items.create :ts => @time.utc
+        item.reload.ts_before_type_cast.should == "2011-04-12 11:30:00.000000"
+      end
+
+      it "writes TimeWithZone objects using the local timezone" do
+        item = @items.create :ts => @time.in_time_zone
+        item.reload.ts_before_type_cast.should == "2011-04-12 11:30:00.000000"
+      end
+
+      it "reads the timestamp in the local timezone (ok)" do
+        item = @items.create :ts => @time
+        ts = item.reload.ts
+        ts.inspect.should == "Tue, 12 Apr 2011 13:30:00 EDT -04:00"
+      end
+
+    end
+
+    context "and time_zone_aware_attributes = false" do
+      before :all do
+        ActiveRecord::Base.time_zone_aware_attributes = false
+        connect_and_define_model
+      end
+
+      it "writes local Time timestamps correctly (ok)" do
+        item = @items.create :ts => @time
+        item.reload.ts_before_type_cast.should == "2011-04-12 11:30:00.000000"
+      end
+
+      it "writes UTC Time in local timezone (ok)" do
+        item = @items.create :ts => @time.utc
+        item.reload.ts_before_type_cast.should == "2011-04-12 11:30:00.000000"
+      end
+
+      it "writes TimeWithZone objects correctly (ok)" do
+        item = @items.create :ts => @time.in_time_zone
+        item.reload.ts_before_type_cast.should == "2011-04-12 11:30:00.000000"
+      end
+
+      it "reads the timestamp as local with the correct offset (ok)" do
+        item = @items.create :ts => @time
+        item.reload.ts.inspect.should == "2011-04-12 11:30:00 -0600"
+      end
+
+    end
+
+  end
+
+end
+
